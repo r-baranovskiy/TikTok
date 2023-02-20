4f201236d2926a32d8f59af66ea31affa332c898
Ruslan <107496218+r-baranovskiy@users.noreply.github.com>
2023-02-20 22:25:12 +0300

    Revert "create post model"

--- TikTok/Controllers/Others/PostViewController.swift
+++ TikTok/Controllers/Others/PostViewController.swift
@@ -1,21 +1,29 @@
+//
+//  PostViewController.swift
+//  TikTok
+//
+//  Created by Кащенко on 20.02.23.
+//
+
 import UIKit
 
-final class PostViewController: UIViewController {
-    
-    private let model: PostModel
-    
-    init(model: PostModel) {
-        self.model = model
-        super.init(nibName: nil, bundle: nil)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError()
-    }
+class PostViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        // Do any additional setup after loading the view.
     }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
