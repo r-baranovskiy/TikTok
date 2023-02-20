9cc19a753d65d791dc5ea7a2d325084dbb79d269
koko6a <rus.baranovskiy@gmail.com>
2023-02-20 16:48:29 +0300

    project init

---
+++ TikTok/AppDelegate.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        let startVC = ViewController()
+        let navVC = UINavigationController(rootViewController: startVC)
+        
+        window.rootViewController = navVC
+        self.window = window
+        self.window?.makeKeyAndVisible()
+        return true
+    }
+}
+
