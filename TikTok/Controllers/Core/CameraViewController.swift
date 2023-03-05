0cabb9e775081b4574bc4d93ca53a8ed1862c60e
koko6a <rus.baranovskiy@gmail.com>
2023-03-06 02:25:59 +0300

    create camera

--- TikTok/Controllers/Core/CameraViewController.swift
+++ TikTok/Controllers/Core/CameraViewController.swift
@@ -1,10 +1,101 @@
 import UIKit
+import AVFoundation
 
-class CameraViewController: UIViewController {
-
+final class CameraViewController: UIViewController {
+    
+    private let captureSession = AVCaptureSession()
+    
+    private var videoCaptureDevice: AVCaptureDevice?
+    
+    private let captureOutput = AVCaptureMovieFileOutput()
+    
+    private var capturePreviewLayer: AVCaptureVideoPreviewLayer? = nil
+    
+    private let cameraView: UIView = {
+        let view = UIView()
+        view.clipsToBounds = true
+        view.backgroundColor = .black
+        return view
+    }()
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setUpView()
+        setUpCamera()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        tabBarController?.tabBar.isHidden = true
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        cameraView.frame = view.bounds
+    }
+    
+    // MARK: - Behaviour
+    
+    private func setUpView() {
+        view.addSubview(cameraView )
         view.backgroundColor = .systemYellow
+        navigationItem.leftBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .close, target: self,
+            action: #selector(didTapClose))
+    }
+    
+    @objc private func didTapClose() {
+        captureSession.stopRunning()
+        tabBarController?.tabBar.isHidden = false
+        tabBarController?.selectedIndex = 0
+    }
+    
+    // MARK: - Camera
+    
+    private func setUpCamera() {
+        if let audioDevice = AVCaptureDevice.default(for: .audio) {
+            let audioInput = try? AVCaptureDeviceInput(device: audioDevice)
+            if let audioInput = audioInput {
+                if captureSession.canAddInput(audioInput) {
+                    captureSession.addInput(audioInput)
+                }
+            }
+        }
+        
+        if let videoDevice = AVCaptureDevice.default(for: .video) {
+            if let videoInput = try? AVCaptureDeviceInput(device: videoDevice) {
+                if captureSession.canAddInput(videoInput) {
+                    captureSession.addInput(videoInput)
+                }
+            }
+        }
+        
+        captureSession.sessionPreset = .hd1280x720
+        
+        if captureSession.canAddOutput(captureOutput) {
+            captureSession.addOutput(captureOutput)
+        }
+        
+        capturePreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        capturePreviewLayer?.videoGravity = .resizeAspectFill
+        capturePreviewLayer?.frame = view.bounds
+        
+        if let layer = capturePreviewLayer {
+            cameraView.layer.addSublayer(layer)
+        }
+        
+        captureSession.startRunning()
+    }
+}
+
+// MARK: - AVCaptureFileOutputRecordingDelegate
+
+extension CameraViewController: AVCaptureFileOutputRecordingDelegate {
+    func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
+        guard error == nil else { return }
+        
+        print("Finished recoring to url \(outputFileURL.absoluteString)")
     }
 }
